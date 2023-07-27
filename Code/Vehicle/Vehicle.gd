cdf9e8e545960543fd6819f22f1ffab9d3bca9a9
PCCDanny <137850298+PCCDanny@users.noreply.github.com>
2023-07-27 13:16:34 -0700

    cars go zroom
    wrapped cars

--- Code/Vehicle/Vehicle.gd
+++ Code/Vehicle/Vehicle.gd
@@ -2,7 +2,12 @@ extends Area3D
 class_name Vehicle
 
 @export var speed: float = 10.0 #m/s
+var bounds: float = 20.0
 
 func _process(delta):
 	position.x -= speed * delta
+	if position.x < -bounds:
+		position.x = bounds
 	
+	if position.x > bounds:
+		position.x = -bounds 
