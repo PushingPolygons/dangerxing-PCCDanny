25678f7c3d8462caae534a2931b569169b86301d
PCCDanny <137850298+PCCDanny@users.noreply.github.com>
2023-07-26 22:39:08 -0700

    Chicken moves lovely
    he slides with gleeee

--- Code/Player/Player.gd
+++ Code/Player/Player.gd
@@ -2,30 +2,37 @@ extends Area3D
 class_name Player
 
 var speed: float = 10.0 # m/s.
-var direction: Vector3
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	area_entered.connect(OnAreaEntered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = Vector2()
+	
+	# Get input from W, A, S, D keys (move_up, move_down, move_left, move_right)
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	
+	direction = direction.normalized() # Normalize the vector to get consistent speed in all directions
+	
 	position += Vector3(direction.x, 0, direction.y) * speed * delta
-#	position.x += 10 * delta
-	print(direction)
 
+	# Check if there is any input to avoid division by zero
+	if direction != Vector2():
+		# Calculate the angle between the current forward direction and the desired direction
+		var angle = atan2(direction.x, direction.y)
+		rotation_degrees.y = rad_to_deg(angle) # Set the Y rotation (heading) in degrees
 
-#	if Input.is_action_pressed("move_left"):
-#		direction = Vector3(-1, 0, 0)
-#	if Input.is_action_pressed("move_right"):
-#		direction = Vector3(-1, 0, 0)
-#	if Input.is_action_pressed("move_up"):
-#		direction = Vector3(0, 0, -1)
-#	if Input.is_action_pressed("move_down"):
-#		direction = Vector3(-1, 0, 1)
-#		position += direction * speed * delta
-#
+func OnAreaEntered(other_area: Area3D):
+	if other_area is Vehicle:
+		kill()
+
+func kill():
+	queue_free()
