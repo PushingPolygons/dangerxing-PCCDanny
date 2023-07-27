e3f8380cdebe10fcb538ce29f5237791f829a695
PCCDanny <137850298+PCCDanny@users.noreply.github.com>
2023-07-26 20:56:35 -0700

    Chicken on the move
    Chickens on the move

--- Code/Player/Player.gd
+++ Code/Player/Player.gd
@@ -1,7 +1,7 @@
 extends Area3D
 class_name Player
 
-var speed: float
+var speed: float = 10.0 # m/s.
 var direction: Vector3
 
 
@@ -12,15 +12,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction: Vector3
-	
-	if Input.is_action_pressed("move_left"):
-		direction = Vector3(-1, 0, 0)
-	if Input.is_action_pressed("move_right"):
-		direction = Vector3(-1, 0, 0)
-	if Input.is_action_pressed("move_up"):
-		direction = Vector3(0, 0, -1)
-	if Input.is_action_pressed("move_down"):
-		direction = Vector3(-1, 0, 1)
-		position += direction * speed * delta
-	
+
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	position += Vector3(direction.x, 0, direction.y) * speed * delta
+#	position.x += 10 * delta
+	print(direction)
+
+
+#	if Input.is_action_pressed("move_left"):
+#		direction = Vector3(-1, 0, 0)
+#	if Input.is_action_pressed("move_right"):
+#		direction = Vector3(-1, 0, 0)
+#	if Input.is_action_pressed("move_up"):
+#		direction = Vector3(0, 0, -1)
+#	if Input.is_action_pressed("move_down"):
+#		direction = Vector3(-1, 0, 1)
+#		position += direction * speed * delta
+#
